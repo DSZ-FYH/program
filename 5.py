b72d3c27cc682f6c8ea62c231d12546be63d33d8
DSZ-FYH <dsz_fengyuhang@163.com>
2023-04-14 18:10:32 +0800

    modified:   5.py

--- 5.py
+++ 5.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 from numpy.linalg import lstsq  # 解超定方程
 from numpy.linalg import solve  # 解线性方程
+from tqdm import tqdm
 
 
 def Euler2Quaternion(pitch, roll, yaw):
@@ -34,7 +35,20 @@ def Quaternion2bCn(q):
         0] * q[3][0]
     return C
 
+def Euler2bCn(pitch, roll, yaw):
+    C = np.zeros((3, 3))
+    C[0, 0] = math.cos(roll) * math.cos(yaw) + math.sin(roll) * math.sin(yaw) * math.sin(pitch)
+    C[1, 0] = -math.cos(roll) * math.sin(yaw) + math.sin(roll) * math.cos(yaw) * math.sin(pitch)
+    C[2, 0] = -math.sin(roll) * math.cos(pitch)
+    C[0, 1] = math.sin(yaw) * math.cos(pitch)
+    C[1, 1] = math.cos(yaw) * math.cos(pitch)
+    C[2, 1] = math.sin(pitch)
+    C[0, 2] = math.sin(roll) * math.cos(yaw) - math.cos(roll) * math.sin(yaw) * math.sin(pitch)
+    C[1, 2] = -math.sin(roll) * math.sin(yaw) - math.cos(roll) * math.cos(yaw) * math.sin(pitch)
+    C[2, 2] = math.cos(roll) * math.cos(pitch)
+    return C
 
+print ("==============参数初始化==============")
 # 参数设置
 IMU_frequency = 200
 GPS_frequency = 1
@@ -132,7 +146,11 @@ X_save = np.zeros((kalman_end, 15))
 P_save = np.zeros((kalman_end, 15))
 sins_save = np.zeros((kalman_end, 3))
 
-for i in range(0, kalman_end):
+print ("==============初始化完成==============")
+
+print ("开始解算...")
+
+for i in tqdm(range(0, kalman_end)):
     # 计算Rm和Rn
     Rm = Re * (1 - 2 * f + 3 * f * math.sin(lat)**2)
     Rn = Re * (1 + f * math.sin(lat)**2)
@@ -152,26 +170,26 @@ for i in range(0, kalman_end):
 
     q1 = np.array([
         np.float64(math.cos(dTheta_norm / 2)),
-        float(-dTheta[0] / dTheta_norm * math.sin(dTheta_norm / 2)),
-        float(-dTheta[1] / dTheta_norm * math.sin(dTheta_norm / 2)),
-        float(-dTheta[2] / dTheta_norm * math.sin(dTheta_norm / 2))
+        float(-dTheta[0][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(-dTheta[1][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(-dTheta[2][0] / dTheta_norm * math.sin(dTheta_norm / 2))
     ])
     q2 = np.array([
-        float(dTheta[0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(dTheta[0][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
         np.float64(math.cos(dTheta_norm / 2)),
-        float(dTheta[2] / dTheta_norm * math.sin(dTheta_norm / 2)),
-        float(-dTheta[1] / dTheta_norm * math.sin(dTheta_norm / 2))
+        float(dTheta[2][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(-dTheta[1][0] / dTheta_norm * math.sin(dTheta_norm / 2))
     ])
     q3 = np.array([
-        float(dTheta[1] / dTheta_norm * math.sin(dTheta_norm / 2)),
-        float(-dTheta[2] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(dTheta[1][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(-dTheta[2][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
         np.float64(math.cos(dTheta_norm / 2)),
-        float(dTheta[0] / dTheta_norm * math.sin(dTheta_norm / 2))
+        float(dTheta[0][0] / dTheta_norm * math.sin(dTheta_norm / 2))
     ])
     q4 = np.array([
-        float(dTheta[2] / dTheta_norm * math.sin(dTheta_norm / 2)),
-        float(dTheta[1] / dTheta_norm * math.sin(dTheta_norm / 2)),
-        float(-dTheta[0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(dTheta[2][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(dTheta[1][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(-dTheta[0][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
         np.float64(math.cos(dTheta_norm / 2))
     ])
     q = np.matmul(np.array([q1, q2, q3, q4]), q)
@@ -185,11 +203,17 @@ for i in range(0, kalman_end):
     # 速度更新
     fb = (IMU[i, 5:8] * 9.8).reshape((IMU[i, 5:8].shape[0], 1)) - acc_bias
     fn = np.matmul(bCn, fb)
+
+    w_ie_n = w_ie_n.reshape((-1, 1))
+    w_en_n = w_en_n.reshape((-1, 1))
+
     # 比力方程
     dv = np.array(fn - np.cross((2 * w_ie_n + w_en_n).reshape(
-        (1, -1)).flatten(), np.array([ve, vn, vu])).reshape(
+        (1, -1)).flatten(), np.array([ve, vn, vu]).reshape((1,-1)).flatten()).reshape(
             (-1, 1)) + np.array([0, 0, -g]).reshape(
                 (np.array([0, 0, -g]).shape[0], 1))) * kalman_gap * IMU_dT
+
+    # print(dv)
 
     ve = ve + dv[0]
     vn = vn + dv[1]
@@ -218,26 +242,26 @@ for i in range(0, kalman_end):
 
     q_sins1 = np.array([
         np.float64(math.cos(dTheta_norm / 2)),
-        float(-dTheta[0] / dTheta_norm * math.sin(dTheta_norm / 2)),
-        float(-dTheta[1] / dTheta_norm * math.sin(dTheta_norm / 2)),
-        float(-dTheta[2] / dTheta_norm * math.sin(dTheta_norm / 2))
+        float(-dTheta[0][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(-dTheta[1][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(-dTheta[2][0] / dTheta_norm * math.sin(dTheta_norm / 2))
     ])
     q_sins2 = np.array([
-        float(dTheta[0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(dTheta[0][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
         np.float64(math.cos(dTheta_norm / 2)),
-        float(dTheta[2] / dTheta_norm * math.sin(dTheta_norm / 2)),
-        float(-dTheta[1] / dTheta_norm * math.sin(dTheta_norm / 2))
+        float(dTheta[2][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(-dTheta[1][0] / dTheta_norm * math.sin(dTheta_norm / 2))
     ])
     q_sins3 = np.array([
-        float(dTheta[1] / dTheta_norm * math.sin(dTheta_norm / 2)),
-        float(-dTheta[2] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(dTheta[1][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(-dTheta[2][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
         np.float64(math.cos(dTheta_norm / 2)),
-        float(dTheta[0] / dTheta_norm * math.sin(dTheta_norm / 2))
+        float(dTheta[0][0] / dTheta_norm * math.sin(dTheta_norm / 2))
     ])
     q_sins4 = np.array([
-        float(dTheta[2] / dTheta_norm * math.sin(dTheta_norm / 2)),
-        float(dTheta[1] / dTheta_norm * math.sin(dTheta_norm / 2)),
-        float(-dTheta[0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(dTheta[2][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(dTheta[1][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
+        float(-dTheta[0][0] / dTheta_norm * math.sin(dTheta_norm / 2)),
         np.float64(math.cos(dTheta_norm / 2))
     ])
     q_sins = np.matmul(np.array([q_sins1, q_sins2, q_sins3, q_sins4]), q_sins)
@@ -247,8 +271,12 @@ for i in range(0, kalman_end):
 
     fb_sins = IMU[i,5:8].reshape((-1,1))
     fn_sins = np.matmul(bCn_sins,fb)
+
+    w_ie_n_sins = w_ie_n_sins.reshape((-1, 1))
+    w_en_n_sins = w_en_n_sins.reshape((-1, 1))
+
     dv = np.array(fn_sins - np.cross((2 * w_ie_n_sins + w_en_n_sins).reshape(
-        (1, -1)).flatten(), np.array([ve_sins, vn_sins, vu_sins])).reshape(
+        (1, -1)).flatten(), np.array([ve_sins, vn_sins, vu_sins]).reshape((1,-1)).flatten()).reshape(
             (-1, 1)) + np.array([0, 0, -g]).reshape(
                 (np.array([0, 0, -g]).shape[0], 1))) * kalman_gap * IMU_dT # 比力方程 
     ve_sins = ve_sins + dv[0]
@@ -278,20 +306,20 @@ for i in range(0, kalman_end):
     F[2,1] = vn/(Rm+h)
     F[2,3] = 1/(Rn+h)*math.tan(lat)
     F[2,6] = Wie*math.cos(lat)+ve/(Rn+h)*1/math.cos(lat)**2
-    F[3,1] = -fn[2]# f_u天向比力
-    F[3,2] = fn[1]# f_n北向比力
+    F[3,1] = -fn[2]     # f_u天向比力
+    F[3,2] = fn[1]      # f_n北向比力
     F[3,3] = (vn*math.tan(lat)-vu)/(Rn+h)
     F[3,4] = 2*Wie*math.sin(lat)+ve/(Rn+h)*math.tan(lat)
     F[3,5] = -(2*Wie*math.cos(lat)+ve/(Rn+h))
     F[3,6] = 2*Wie*math.cos(lat)*vn+ve*vn/(Rn+h)*1/math.cos(lat)**2+2*Wie*math.sin(lat)*vu
-    F[4,0] = fn[2]# f_u天向比力
-    F[4,2] = -fn[0]# f_e东向比力
+    F[4,0] = fn[2]      # f_u天向比力
+    F[4,2] = -fn[0]     # f_e东向比力
     F[4,3] = -(2*Wie*math.sin(lat)+ve/(Rn+h)*math.tan(lat))
     F[4,4] = -vu/(Rm+h)
     F[4,5] = -vn/(Rm+h)
     F[4,6] = -(2*Wie*math.cos(lat)+ve/(Rn+h)*1/math.cos(lat)**2)*ve
-    F[5,0] = -fn[1]# f_n北向比力
-    F[5,1] = fn[0]# f_e东向比力
+    F[5,0] = -fn[1]     # f_n北向比力
+    F[5,1] = fn[0]      # f_e东向比力
     F[5,3] = 2*(Wie*math.cos(lat)+ve/(Rn+h))
     F[5,4] = 2*vn/(Rm+h)
     F[5,6] = -2*ve*Wie*math.sin(lat)
@@ -302,6 +330,62 @@ for i in range(0, kalman_end):
     F[0:3,9:12] = bCn
     F[3:6,12:15] = bCn
 
-    print(F)
+    # 状态转移矩阵F离散化
+    F = np.eye(15)+F*IMU_dT+np.linalg.matrix_power(F,2)*IMU_dT**2/2+np.linalg.matrix_power(F,3)*IMU_dT**3/6
 
-    print("hello")
+    G = np.zeros((15,6))
+    G[0:3,0:3] = bCn
+    G[3:6,3:6] = bCn
+
+    H = np.zeros((6,15))
+    H[0:3,3:6] = np.eye(3)
+    H[3:6,6:9] = np.eye(3)
+    # H[0:6,3:9] = np.eye(6)
+
+    Q = np.linalg.matrix_power(np.diag(np.array([np.ones(3)*np.deg2rad(0.1/3600),np.ones(3)*50e-6*g]).reshape(1,-1).flatten()),2)
+
+    R_temp = np.array([np.ones(1)*0.1/(Rn*math.cos(lat)),np.ones(1)*0.1/Rm,np.ones(1)*0.15]).reshape(1,-1).flatten()
+    R = np.linalg.matrix_power(np.diag(np.array([np.ones(3)*0.01,R_temp]).reshape(1,-1).flatten()),2)
+
+    Z =np.append(np.array([ve,vn,vu]).reshape(1,-1).flatten()-GPS[i,5:8],np.array([lat,lon,h]).reshape(1,-1).flatten()-GPS[i,2:5]).reshape(-1,1)
+
+    X_k_k_1 = np.matmul(F,X_k)
+    P_k_k_1 = np.matmul(np.matmul(F,P_k),F.T)+np.matmul(np.matmul(G,Q),G.T)
+    K_k = np.matmul(np.matmul(P_k_k_1,H.T),np.linalg.inv(np.matmul(np.matmul(H,P_k_k_1),H.T)+R))
+    X_k = X_k_k_1 + np.matmul(K_k,Z-np.matmul(H,X_k_k_1))
+    P_k = np.matmul(np.eye(15)-np.matmul(K_k,H),P_k_k_1)
+
+    # 状态量（误差）保存
+    X_save[i,:] = X_k.T
+
+    # P阵对角线保存
+    P_save[i,:] = np.diag(P_k).reshape(1,-1)
+
+    # 姿态修正
+    bCn = np.matmul(Euler2bCn(X_k[0],X_k[1],X_k[2]),bCn)
+    yaw = np.rad2deg(math.atan2(bCn[0,1], bCn[1,1]))
+    roll = np.rad2deg(math.atan2(-bCn[2,0], bCn[2,2]))
+    pitch = np.rad2deg(math.asin(bCn[2,1]))
+
+    # 速度修正
+    ve = ve[0] - X_k[3][0]
+    vn = vn[0] - X_k[4][0]
+    vu = vu[0] - X_k[5][0]
+
+    # 位置修正
+    lat = lat[0] - X_k[6][0]
+    lon = lon[0] - X_k[7][0]
+    h = h[0] - X_k[8][0]
+
+    gyro_bias = X_k[9:12]
+    acc_bias = X_k[12:15]
+
+    # 误差清零
+    X_k[0:9] = np.zeros((9,1))
+    
+    # 滤波数据保存
+    data_save[i,:] = np.array([lat,lon,h,pitch,roll,yaw,ve,vn,vu])
+    
+
+print ("解算完成！")
+print ("开始绘图")
